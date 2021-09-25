6fd3b8fd2181d74a1556c3be5c309ed47873817e
KJstudio1171 <june1171@naver.com>
2021-09-25 23:58:43 +0900

    러쉬
    러쉬

---
+++ rush-02/rush02.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junekim <june1171@naver.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/25 23:04:25 by junekim           #+#    #+#             */
+/*   Updated: 2021/09/25 23:38:18 by junekim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+	char number[691];
+	int fp = open("./numbers.dict", O_RDONLY);
+	int len = 0;
+	int i = -1;
+	while(read(fp,number,691))
+		len++;
+	while(++i < 692)
+	printf("%d", number[690]);
+	if(close(fp) == -1)
+		printf("error");
+}
