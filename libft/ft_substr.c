e1ca2eb94579fc3fe5bdbc018460422e4c1b6ca8
KJstudio1171 <june1171@naver.com>
2022-01-19 00:04:54 +0900

    error find

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: junekim <june1171@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 14:24:30 by junekim           #+#    #+#             */
-/*   Updated: 2022/01/15 00:08:35 by junekim          ###   ########seoul.kr  */
+/*   Updated: 2022/01/19 00:02:07 by junekim          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*str;
 
+	if (!s)
+		return (NULL);
+	if ((unsigned int) ft_strlen(s) < start)
+		return (ft_strdup(""));
 	i = 0;
 	j = 0;
-	str = (char *) malloc(len * sizeof(char) + 1);
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	str = (char *) malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	while (s[i])
-	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
-		i++;
-	}
-	str[j] = '\0';
+	ft_strlcpy(str, s + start, len + 1);
 	return (str);
 }
