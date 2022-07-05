272a7df3b2dc003ec0b8604f95314cbd0243dd6d
KJstudio1171 <june1171@naver.com>
2022-07-05 21:40:11 +0900

    push_swap add

---
+++ push_swap/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junekim <june1171@naver.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/30 11:19:18 by junekim           #+#    #+#             */
+/*   Updated: 2022/01/14 22:31:17 by junekim          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(n * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, n * size);
+	return (ptr);
+}
---
+++ push_swap/libft/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junekim <june1171@naver.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/28 18:00:21 by junekim           #+#    #+#             */
+/*   Updated: 2022/01/23 11:02:48 by junekim          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int				i;
+	unsigned char	chr;
+
+	i = 0;
+	chr = (unsigned char)c;
+	while (str[i])
+	{
+		if (str[i] == chr)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (str[i] == chr)
+		return ((char *)&str[i]);
+	return (NULL);
+}
